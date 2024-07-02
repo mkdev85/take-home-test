7e6e9bc3deaa4f88d6c1d806cc01758185cfaadf
Mayank <mkdev8585@gmail.com>
2024-07-02 11:27:49 +0530

    Setup basic routes structure.

---
+++ src/routes/api/v1/book.router.ts
@@ -0,0 +1,11 @@
+// import express from 'express';
+
+// import bookController from 'src/controllers/book.controller';
+// import Authenticate from 'src/middlewares/authenticate';
+
+// const args = { mergeParams: true };
+// const bookRouter = express.Router(args);
+
+// bookRouter.route('/').post(bookController.GetAllTodos);
+
+// export { bookRouter };

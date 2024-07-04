c8bc160521b52badaf76c0a861398bf296a10759
Mayank <mkdev8585@gmail.com>
2024-07-04 11:56:56 +0530

    Added migration for implementing unique constraint on author.

---
+++ src/migrations/1720071714888-Migration.ts
@@ -0,0 +1,13 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Migration1720071714888 implements MigrationInterface {
+  name = 'Migration1720071714888'
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "authors" ADD CONSTRAINT "UQ_6c64b3df09e6774438aeca7e0b0" UNIQUE ("name")`);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "authors" DROP CONSTRAINT "UQ_6c64b3df09e6774438aeca7e0b0"`);
+  }
+}
